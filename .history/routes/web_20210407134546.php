577501d730ffa95313d0960f2fce7901dcd46ad3
liuje-hf <1815813083@189.cn>
2021-04-07 06:05:33 +0000

    ³õʼ»¯Ñùʽ

--- .history/routes/web_20210407134546.php
+++ .history/routes/web_20210407134546.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', function () {
+    return view('welcome');
+});

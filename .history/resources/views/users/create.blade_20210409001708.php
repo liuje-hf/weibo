20df34dcaf5956c53445bce2aee7c29d02818863
liuje-hf <1815813083@189.cn>
2021-04-08 16:22:04 +0000

    增加CSRF验证

--- .history/resources/views/users/create.blade_20210409001708.php
+++ .history/resources/views/users/create.blade_20210409001708.php
@@ -28,7 +28,7 @@
 
         <div class="form-group">
           <label for="password_confirmation">确认密码：</label>
-          <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+          <input type="password_confirmation" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
         </div>
 
         <button type="submit" class="btn btn-primary">注册</button>

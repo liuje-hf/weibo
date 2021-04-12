34ba450ebdc31960bb75931b40cd6ac986d6ffe4
liuje-hf <1815813083@189.cn>
2021-04-12 03:33:43 +0000

    重置密码

--- .history/app/Http/Controllers/PasswordController_20210412112746.php
+++ .history/app/Http/Controllers/PasswordController_20210412112746.php
@@ -60,7 +60,7 @@ class PasswordController extends Controller
         $request->validate([
             'token' => 'required',
             'email' => 'required|email',
-            'password' => 'required|confirmed|min:6',
+            'password' => 'required|confirmed|min:8',
         ]);
         $email = $request->email;
         $token = $request->token;
